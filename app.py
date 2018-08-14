1f5bb50d03688b1c873855961b1a11ee7f316dbd
Vanessa Van Gilder <vkshiro@live.com>
2018-08-14 15:19:56 -0400

    Fix route issues

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import (Flask, jsonify, render_template, 
                    send_file, url_for, send_from_directory)
-import json
 import os
 
 
@@ -10,10 +9,10 @@ app = Flask(__name__)
 '''Define the Routes'''
 
 #favicon
-@app.route('/favicon.ico')
+'''@app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static/img/'),
-                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
+                          'favicon.ico',mimetype='image/vnd.microsoft.icon')'''
 
 
 #index
@@ -29,21 +28,13 @@ def cloud(word_cloud):
 #chord diagram
 @app.route('/chord')
 def chord():
-
-    with open('static/data/chord-data.json') as filename:
-        chordData = json.load(filename)
-
-    return (jsonify(chordData))   
+    return send_file('static/data/chord-data.json', mimetype='application/json')
 
 #model bias predictor
 @app.route('/model')
 def model():
-
-    with open('static/data/model-data.json') as filename:
-        modelData = json.load(filename)
-
-    return (jsonify(modelData)) 
-
+    return send_file('static/data/model-data.json', mimetype='application/json')
+  
 #run the app
 if __name__ == '__main__':
     app.run(debug=True)
