a8ccfff197ef951d386ab38066411c9b8f6b88bf
VanessaVanG <vkshiro@live.com>
2018-08-21 10:25:25 -0400

    Clean up app

--- app.py
+++ app.py
@@ -2,18 +2,10 @@ from flask import (Flask, jsonify, render_template,
                    send_file, url_for, send_from_directory)
 import os
 
-
 #make an app instance
 app = Flask(__name__)
 
 '''Define the Routes'''
-
-#favicon
-'''@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static/img/'),
-                          'favicon.ico',mimetype='image/vnd.microsoft.icon')'''
-
 
 #index
 @app.route('/')
